980c500b57038980459068a90a4f71634d05980c
james-marshall314 <james.marshall314@gmail.com>
2015-08-17 15:11:02 -0400

    passing

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  case array.size
+  when 0
+    #do nothing, this would be an error. 
+  when 1
+    array[0]
+  when 2
+    "#{array[0]} and #{array[1]}"
+  else
+    last = array.pop
+    string = array.join ", "
+    string << ", and #{last}"
+  end
 end
