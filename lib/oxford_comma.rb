5601e62f9d5e9fe2af8e1ebb5010bf40bba1fbe9
Ismail Mustafa <ismailmustafa@rocketmail.com>
2015-08-19 09:44:30 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+    if array.length == 0
+        return
+    elsif array.length == 1
+        array.pop
+    elsif array.length == 2
+        array.shift + " and " + oxford_comma(array)
+    elsif array.length >= 3
+        result = String.new
+        (0..array.length-1).each do |index|
+            if index == array.length-1
+                result += "and #{array[index]}"
+            else
+                result += "#{array[index]}, "
+            end
+        end
+        result
+    end
 end
