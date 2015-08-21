8fa3e1f2f62624e2b2deef0ab93a1add7140e323
jeffslutzky <jeffslutzky@gmail.com>
2015-08-21 16:43:03 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  phrase = "#{array[0]}"
+  length = array.length
+  if length == 2
+    phrase << " and #{array[1]}"
+  elsif length > 2
+    for word in 1..length-2 do
+      phrase << ", #{array[word]}"
+    end
+    phrase << ", and #{array[length-1]}"
+  end
+  phrase
 end
