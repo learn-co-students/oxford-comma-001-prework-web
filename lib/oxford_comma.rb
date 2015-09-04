4553a7e34d7c464dffb6850659ede4ff0ff15fa0
mgsterling11 <m.g.sterling11@gmail.com>
2015-09-04 14:38:11 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
-def oxford_comma(array)
+array = ["kiwi","durian","starfruit", "mangos", "dragon fruits"]
 
+def oxford_comma(array)
+  return array[0] if array.length == 1
+  return array.join (" and ") if array.length ==2
+  return array[0..-2].join(', ') + ", and " + array.last
 end
