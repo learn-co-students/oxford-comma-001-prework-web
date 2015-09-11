a62ed0224933798775bfa2b03d9d45575e629151
pedrotp <pedro@pedrotp.com>
2015-09-11 12:46:42 -0400

    oxfordcommatized

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else 
+    new_element = array.pop
+    array.push("and #{new_element}")
+    array.join(", ")
+  end
 end
