019987ba635a1a65e8af52d6f2da7dcc4050c2b9
namwoody <deepsarora262@gmail.com>
2015-09-13 00:14:15 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+   case array.size
+  when 0
+   
+when 1
+  array[0]
+when 2
+  "#{array[0]} and #{array[1]}"
+else
+  last = array.pop
+  string = array.join ", "
+  string << ", and #{last}"
+   end
+    
 end
