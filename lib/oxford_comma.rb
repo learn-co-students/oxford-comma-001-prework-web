8133e1e888988622c05d83e33a9823a4e8a08de5
skipsuva <skip.suva@gmail.com>
2015-09-16 07:14:59 -0400

    I'm a renowned language scholar

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  if array.count >= 3
+    
+    array.insert(array.count-1, "")
+    array2 = array.pop(2)
+    array.join(", ") << array2.join(", and ")
+      
 
+  elsif array.count == 2
+    array.insert(1, " and ")
+    array.join
+
+
+  else
+    array.join
+  end
+  
 end
