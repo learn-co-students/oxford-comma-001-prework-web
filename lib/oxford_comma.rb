e31d30617658973e0057fd76be6aab3708a850b8
mirc89 <kmircovich1@gmail.com>
2015-09-16 21:13:21 -0400

    grammar in check

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+  else
+    last_index = array.length #4
+    the_string = ""
+    #Add comma and space to each element except for last element.
+    array.each_with_index do |item, index|
+      if index < array.length - 1
+        the_string << item
+        the_string += ", "
+      end
+      if index == array.length - 1
+        the_string += "and "
+        the_string << item
+      end
+    end
+    the_string
+  end
 end
