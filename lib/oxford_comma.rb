cb642cdbb2ba9645fbf3a381bbb2d9807d234ba1
dunxtand <mdrstandish@gmail.com>
2015-09-18 13:34:43 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  sentence = ""
+  if array.length == 1
+    sentence = array[0]
+  elsif array.length == 2
+    sentence = array[0] + " and " + array[1]
+  else
+    (0..(array.length-2)).each { |index| sentence += (array[index] + ", ") }
+    sentence += ("and " + array[-1])
+  end
+  sentence
 end
