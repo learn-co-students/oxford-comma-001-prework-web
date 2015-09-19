c3d45b10ba6e913f9a012c80c34e95893c3f03f1
benstew <benstewart2008@gmail.com>
2015-09-18 22:59:39 -0400

    All DONE

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
-
+def oxford_comma(array, *others)
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array[0] + " and " + array[1]
+  else
+    array[0...-1].join(", ") + ", and " + array[-1].to_s
+  end
 end
