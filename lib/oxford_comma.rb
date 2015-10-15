d4ee414508640f012713eaf3bd7d93f938215a16
Dylan Brevis <dylanito2@gmail.com>
2015-10-15 17:01:46 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  str = ""
+  if array.size == 1
+    str = array[0]
+  elsif array.size == 2
+    str = "#{array[0]} and #{array[1]}"
+  elsif array.size > 2
+    working = array[0...-1] 
+    str = working.join(", ")
+    str += ", and #{array[-1]}"
+  end
 end
