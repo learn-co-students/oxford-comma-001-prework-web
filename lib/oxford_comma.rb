6df420ccff853e8e6a22b966e4b58c3d6b1b00ab
flats <dave@robador.com>
2015-10-18 03:52:04 -0400

    use oxford comma correctly

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  response = ""
+  if array.length < 3
+    response = array.join(" and ")
+  else
+    array[-1].prepend("and ")
+    response = array.join(", ")
+  end
+  response
 end
