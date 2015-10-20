200a19ceb85217679aeadb11ac16e9b040bbf608
mikeappell <mikeappell@gmail.com>
2015-10-20 21:43:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
 def oxford_comma(array)
-
+  if array.length == 1 then array[0]
+  elsif array.length == 2 then "#{array[0]} and #{array[1]}"
+  else array.collect {|item| item == array.last ? "and #{item}" : "#{item}, "}.join("")
+  end
 end
