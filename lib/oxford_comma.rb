5e514f39c7a2251e37ed0f19feb39e8f67de2560
kaddae <kai@tilde.io>
2015-10-29 15:55:41 -0700

    comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  list=""
+  if array.length==1
+    list<< array.first
+    return list
+  elsif array.length==2
+    list<< array.first + " and "
+    list<< array.last
+  else last=array.pop
+    array.each do |word|
+      list<< "#{word}, "
+    end
+    list << "and #{last}"
+  end
 end
