888d10fb4435c4ecccbcccbc8133d004027688f3
daniellacor <daniella.corricelli@gmail.com>
2015-10-30 15:17:36 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+      array.join
+    elsif array.length == 2
+      two_array = array.join(" and ")
+    else 
+      last_element = array[-1].size + 1
+      joint_string = array.join(", ")
+      joint_string.insert(-last_element, "and ")
+    end
 end
