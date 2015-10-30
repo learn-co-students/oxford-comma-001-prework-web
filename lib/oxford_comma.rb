53c3a4e47873d1c2e9c793203c52ffed44f9686e
KhalilC <klcrumpler@gmail.com>
2015-10-30 08:05:21 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+def three_or_more(array)
+    array[-1] = "and " + array.last
+    array = array.join(", ")
+    return array
+  end
 
+
+
+
+def oxford_comma(array)
+  return array.join("") if array.count == 1
+  return array.join(" and ") if array.count == 2
+  return three_or_more(array) if array.count >= 3
 end
