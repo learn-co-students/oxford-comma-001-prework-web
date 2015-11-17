7679d21ad85d01a42697d928dee1d4b00ef49ae7
juliascript <juliageist23@gmail.com>
2015-11-17 20:16:13 +0000

    Oxford comma lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  size = array.size
+  if size == 1
+    array[0]
+  elsif size == 2
+    array.join(" and ")
+  else
+    last = array.pop
+    array = array.join(", ")
+    array = array + ", and " + last
+    array
+  end
 end
