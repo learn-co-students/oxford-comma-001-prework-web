6d01175b34dfcd076ffe71d0204eae8447c3106a
tuckerbohman5 <tucker.bohman@gmail.com>
2015-11-16 22:26:18 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+
 def oxford_comma(array)
+  if array.count == 1
+    result = array[0]
+  elsif array.count == 2
+    result = array.join(" and ")
+  else
+    result = "#{array[(0...-1)].join(", ")}, and #{array.last}"
+  end
 
 end
