9208fb3df0f8cc34e6ba7706737625670100a07c
ccfz <casparfz@gmail.com>
2015-11-23 14:02:55 +0100

    done with array

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+require 'pry'
 
+
+array_1= ["Book", "Movie", "House", "Tree"]
+
+
+def oxford_comma(array)
+  if array.size == 1
+    array.join(",")
+  elsif array.size == 2 
+    array.join(" and ")
+  else array.size >= 3
+    i=array.size
+    string=array[0..i-2].join(", ")
+    string << ", and #{array.last}"
+  end
 end
+
