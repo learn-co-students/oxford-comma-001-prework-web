22f586a0edb90a595e1ece44046c0a218c1961e8
Aemrox <adam.may2164@gmail.com>
2015-11-23 23:43:23 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  output = ""
+  if array.length == 2
+    return array[0] + " and " + array[1]
+  else  
+    for x in 0..(array.length - 1) do 
+      if x == 0
+        output += array[x]
+      elsif x == (array.length - 1)
+        output += ", and " + array[x]
+      else
+        output+= ", " + array[x]
+      end
+    end
+  end  
+  return output
 end
