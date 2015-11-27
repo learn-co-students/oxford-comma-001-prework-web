2db57e4020cdc6b960a5af23b06d1667f8b51bd1
wuandy91 <wuandy91@gmail.com>
2015-11-27 00:22:11 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+def oxford_comma(fruits)
+	if fruits.length == 1
+		return fruits.join
+	elsif fruits.length == 2
+		return fruits.join(" and ")
+	elsif fruits.length > 2
+		string = ""
+		i=0
+		while i < fruits.length
+	    	if i < (fruits.length - 1)
+	   		string << fruits[i] + ", "
+	   		elsif i >= (fruits.length - 1)
+	   		string << "and " + fruits[i]
+	   		end
+		i+=1
+		end
+	string
+	end
+			
 
 end
