33db98fac1e88a8399e09b29d5b43c61090c56be
danfaeh <danfaeh@gmail.com>
2015-12-02 13:15:26 -0800

    all done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join(", ")
+  elsif array.size == 2
+    array.join(" and ")
+  else 
+    a = array.pop
+    b = array.join(", ")
+    b + ", and " + a
+  end      
 end
+
+#oxford_comma(["a","b","c"])
