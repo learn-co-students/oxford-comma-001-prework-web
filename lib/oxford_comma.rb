a8f747e31d3d1c423230b7239a8de7d0bfabff47
WilliamAG <william.a.gough@gmail.com>
2015-12-02 22:58:28 -0500

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
 
+
+def oxford_comma(a)
+  if a.length == 0
+    ""
+  elsif a.length==1
+    a.first
+  elsif a.length==2
+    a.join(" and ")
+  else
+    last= a.pop
+    "#{a.join(", ")}, and #{last}"
+  end
 end
+  
