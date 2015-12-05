16072b1b232817e9018d5674490a2e4b50681109
dchome <daniel.homer6@gmail.com>
2015-12-05 12:42:08 -0500

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  count = array.size
+    if count == 1
+      array.first
+    elsif count == 2
+      array.join(" and ")
+    else
+      array[-1] = "and #{array[-1]}"
+      array.join(", ")
+    end
 end
