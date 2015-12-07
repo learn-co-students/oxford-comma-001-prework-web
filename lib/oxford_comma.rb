75f442e2d5d0568181fa3cc6fadae61e8ba00eaa
devinburnette <devin.burnette@gmail.com>
2015-12-07 08:12:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  size = array.size
+  if size == 1
+    array[0]
+  elsif size == 2
+    array.join(" and ")
+  elsif size >= 3
+    last = array[-1]
+    array[-1] = "and " + last
+    array.join(", ")
+  end
 end
