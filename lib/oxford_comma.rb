90c1dabebdc414a309c040ed34165059d1245c57
Adam King <adam.king0126@gmail.com>
2015-12-08 23:45:57 +0000

    finish exercise

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+  elsif array.length > 2
+    string = ""
+    array.each_with_index do |element, index|
+      string += element
+      if index == array.length - 2
+        string += ", and "
+      elsif index < array.length - 1
+        string += ", "
+      end
+    end
+    string
+  end
 
 end
