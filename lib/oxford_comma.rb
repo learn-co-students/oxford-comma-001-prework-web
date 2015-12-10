f2fedfae2be566805fcccc3f4bb4e80532437523
Shena Yoshida <sheyoshida@gmail.com>
2015-12-10 13:35:16 -0500

    finished

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        #do nothing
+        elsif array.length == 2
+        array.insert(-2, "and")
+        return array.join(" ")
+        else
+        array[-1] = "and #{array[-1]}"
+    end
+    array.join(", ")
 end
