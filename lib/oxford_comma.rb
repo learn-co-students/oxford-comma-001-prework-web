6b1fb7a79af47169739d52045ddfc0d93168c65b
zacscodingclub <zbaston@gmail.com>
2015-12-12 15:15:47 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    ending = array.pop
+    message = array.join(", ")
+    message << ", and #{ending}"
+  end
 end
