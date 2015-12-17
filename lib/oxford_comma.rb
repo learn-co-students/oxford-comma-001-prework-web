dbb10a8633e43401d06fa496b7ff906893ed7a0e
Shirley Berry <shirleylberry@gmail.com>
2015-12-17 21:18:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        string = array[0]
+    elsif array.length == 2
+        string = array.join(" and ")
+    elsif array.length > 2
+        last_word = array.pop
+        string = array.join(", ")
+        string << ", and #{last_word}"
+    end
+    string
 end
+
+# puts oxford_comma(["Hello", "world", "hi", "there"])
+puts oxford_comma(["hello"])
