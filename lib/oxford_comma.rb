ed9df97c3629d0606535163a731f0c3e68b4b56c
Alex Montolio <alex.montolio@gmail.com>
2015-12-18 06:57:34 -0500

    Doing it with '.each'

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+	size = array.length	
+	if size == 1
+		return array[0]
+	elsif size == 2
+		return array.join(" and ")
+	else
+		string = ""
+		array.each do |x|
+			if x == array.last
+				string << "and #{x}"
+				break
+			end
+			string << "#{x}, "
+		end
 
+		return string
+	end
 end
