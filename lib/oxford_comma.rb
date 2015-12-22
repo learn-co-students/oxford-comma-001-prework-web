340e24611c48261da5a10a44d7aa38ba27e7af33
nsantac <santacruz0605@gmail.com>
2015-12-22 10:32:22 -0500

    array converted to string complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.count == 1
+        return array.join
+    elsif array.count == 2
+        return array.join(" and ")
+    else
+        array.insert(-1, "and #{array.last}")
+        array.delete_at(-2)
+        return strg = array.join(", ")
+    end
 end
