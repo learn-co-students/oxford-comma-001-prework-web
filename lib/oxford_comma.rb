ae453ccadfe66c0a4c941e018ad497356b78f080
chintas1 <chintas1@tcnj.edu>
2015-12-23 18:57:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 
   if(array.size>2)
     str_end = array.pop
-    array.join(", ") + ", and #{str_end}"
+    "#{array.join(", ")}, and #{str_end}"
   elsif(array.size==2)
     array.join(" and ")
   else
