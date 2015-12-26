9459dcbbadeca22c99ad5d844cfa612875789e1a
dougtebay <dougtebay@gmail.com>
2015-12-26 15:50:10 -0500

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0].to_s
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    string = String.new
+    array.each_with_index do |word, index|
+      index < array.length - 1 ? string << "#{word}, " : string << "and #{word}"
+    end
+    string
+  end
 end
