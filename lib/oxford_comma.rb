d628f148a38b9da597221150890834c598e382df
evzaro <evzaro@gmail.com>
2015-12-28 15:34:46 -0500

    Oxford Comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+def oxford_comma(arr)
+
+if arr.length == 1
+  arr[0]
+elsif arr.length == 2
+  "#{arr[0]} and #{arr[1]}"
+else
+  arr[-1] = "and #{arr[-1]}"
+  arr.join(", ")
+end
 
 end
