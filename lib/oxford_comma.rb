7183027668b1e3802e6ad8e1c9f838bf811c834c
harichandansingh <singh.harichandan@gmail.com>
2015-12-27 23:29:34 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    oxford = String.new
+    array.each do|item|
+     if item == array.last
+      oxford << "and #{item}"
+      break
+    end
+    oxford << "#{item}, "
+  end
+  oxford
+  end
 end
