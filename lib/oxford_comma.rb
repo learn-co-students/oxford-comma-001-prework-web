cc35ca5a0a83e836cb1f7a984896f03d8f166f44
Abowlofapples <dominiquealiciabrown@gmail.com>
2016-01-02 23:07:58 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array.join
+  end
+  if array.length == 2
+    return array.join(" and ")
+  end
+  if array.length == 3
+    return array.join(", ").insert(14, 'and ')
+  end 
+  if array.length > 3
+    return array[0...-1].join(", ") + ', and ' + array[ -1]
+  end
 end
