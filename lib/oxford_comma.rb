73c0f8e858916652af2125c1fe860a303dcd423f
mindyyuan <mindyuan@gmail.com>
2016-01-08 11:00:48 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    array[array.length-1] = "and " + array[array.length-1]
+    return array.join(" ")
+  else  
+    array[array.length-1] = "and " + array[array.length-1]
+    return array.join(", ")
+  end
 end
