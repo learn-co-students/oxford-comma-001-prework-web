c8efdfa877aa0ec64cffb60492f99b02ed9249ae
fossbender <fossbender@gmail.com>
2016-01-10 17:43:18 +0000

    Done.

---
+++ lib/array-type-conversion.rb
@@ -0,0 +1,8 @@
+require 'pry'
+puts animal_array = "hippo giraffe monkey horse".split(" ")
+
+puts number_array = (1..3).to_a
+
+puts animal_array.join(" :-),  ")
+
+#binding.pry
--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array[0]
+  elsif array.size == 2
+      array.join(' and ')
+  elsif array.size > 2
+    last_item = array.pop
+    fruit_list = array.join(', ')
+    fruit_list << ", and #{last_item}"
+    array.push(last_item)
+    return fruit_list
+  end
 end
