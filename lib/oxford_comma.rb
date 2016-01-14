2b4eb2719dd2610cc0ae616535cebd388a4f90cb
HarlemSquirrel <wittyman37@yahoo.com>
2016-01-14 22:42:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
+  n = array.length
+  return array[0] if n < 2
+  last = array.pop if n > 1
+  string = array.join(", ")
+  string << "," if n > 2
+  string << " and #{last}" if n > 1
 
 end
