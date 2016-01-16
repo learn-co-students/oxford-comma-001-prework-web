110bb77fd465b989f82288e9694ebf30d87d5f11
Sebastian Scholl <sebscholl@gmail.com>
2016-01-16 19:12:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  return array.join if array.count == 1
+  if array.count == 2
+    array.join(" and ")
+  else
+    last = array.last
+    array.pop
+    array.push("and #{last}")
+    array.join(", ")
+  end
 end
