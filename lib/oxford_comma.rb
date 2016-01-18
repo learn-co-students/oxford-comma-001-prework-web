2ad4d1230dca735522e194b6ab77668c3d3532dd
orobert1 <orobert@risd.edu>
2016-01-18 16:56:01 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+  string = ""
+  array.each_with_index do |item,index|
+    if index == 0
+      string+=item
+    elsif array.length>2&&index!=array.length-1
+      string+=", #{item}"
+    elsif index==array.length-1&&array.length>2
+      string+=", and #{item}"
+    elsif index==array.length-1&&array.length<=2
+      string+=" and #{item}"
+    end
+
+      
+
+
+
+  end
+
+    return string
 
 end
