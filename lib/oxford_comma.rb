b31f6c88ee379d01c24a0820873d4794aa5ca3e1
chrism81 <chrism81@msn.com>
2016-01-19 12:29:56 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,38 @@
 def oxford_comma(array)
+  x = ''
+  y=array.pop
+  if array.empty? == false
+    if array.count==1
+     x.concat("#{array.pop} and #{y}")
+     return x 
+     puts x
+    else
+    array.each { |e|  x.concat("#{e}, ")}
+      x.concat("and #{y}")
+      return x 
+      puts x
+    end
+  end
 
+  if array.empty? == true
+   return y
+  end
 end
+  
+  #elsif array.count>=2
+    
+  #  puts array.join(", ")
+    #x=[]
+    #array.each {|e| x << "#{e}, "}
+    
+    #x.join
+    #puts x
+    #puts array
+  #  puts "and #{y}"
+ # end
+
+
+oxford_comma(["kiwi"])
+oxford_comma(["kiwi", "durian"])
+oxford_comma(["kiwi", "durian", "starfruit"])
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])
