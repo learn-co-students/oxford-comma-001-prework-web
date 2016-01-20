6f981aa74ac675b4868f1d1985cd5fa43e4cecf4
eessex <eve.essex@gmail.com>
2016-01-20 03:43:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-  puts array.join(", ")
+  if array.size == 1
+     array.first
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    "#{array[0, array.size-1].join(", ")}, and #{array.last}"
+  end
 end
