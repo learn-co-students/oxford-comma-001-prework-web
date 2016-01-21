13e53ebbf1f81431f619d91ec846a0a1a5ede744
danielperzales <dani3l122@hotmail.com>
2016-01-21 17:58:38 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+ if array.size == 1 #returns string
+     array.join
+   elsif array.size == 2 #returns a string with elements separated by "and"
+     array.join(" and ")
+   elsif array.size > 2 #returns a string separated by commas, with "and" before the last element
+     last = array.pop #remove the last element
+     output = array.join(", ") #convert array into a string
+     output.concat(", and #{last}") #now append the removed element with ", and" to the string
+   end
+  end        
 
-end
+
+
+
+
+
