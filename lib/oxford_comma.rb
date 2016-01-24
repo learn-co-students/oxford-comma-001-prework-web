7c7d5a7a5e1eb74eb47e57e096bc16cc3858f4fc
jjseymour <jjseymour@gmail.com>
2016-01-24 12:41:17 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    string_last = array.last
+    array.pop
+    string_comma = array.join(", ")
+    "#{string_comma}, and #{string_last}"
+  end
+
 
 end
