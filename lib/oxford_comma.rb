328fd9bb9e6732a997a00f1fd631144000eb163b
Brian Bier <briansbier@gmail.com>
2016-02-05 14:37:09 -0500

    Solution complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length <= 1
+    array.join(',')
+  elsif array.length <= 2
+    array.join(' and ')
+  else
+    slice_number = array.slice!(-1)
+    array.join(',').split(',').insert(-1,'and ').join(', ') + slice_number.to_s
+  end
 end
+
+p oxford_comma([1,2,3,4,5])
