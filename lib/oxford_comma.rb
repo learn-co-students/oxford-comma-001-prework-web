89a878eb4905b7bac8aacc887c6d38f0e4d36c4b
wlschnee <will@willschneekloth.com>
2016-02-05 22:45:17 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  sentence = ""
+  if array.length == 1
+    sentence = array.join("")
+  elsif array.length == 2
+    sentence = array.join(" and ")
+  else
+    while array.length > 2
+      comma = array.shift.to_s
+      sentence += "#{comma}, "
+    end
+    oxford = array.join(", and ")
+    sentence += oxford
+  end
+  return sentence
 end
