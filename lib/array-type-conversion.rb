69b8a8ee43ff65bc28b20702e058eaf8be12d8a9
fossbender <fossbender@gmail.com>
2016-02-06 11:00:11 +0000

    nitrous-backup at Sat Feb  6 11:00:11 UTC 2016

--- lib/array-type-conversion.rb
+++
@@ -1,8 +0,0 @@
-require 'pry'
-puts animal_array = "hippo giraffe monkey horse".split(" ")
-
-puts number_array = (1..3).to_a
-
-puts animal_array.join(" :-),  ")
-
-#binding.pry
--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,27 @@
+require 'pry'
+comma = ["kiwi", "durian", "starfruit"]
+
 def oxford_comma(array)
   if array.size == 1
-    array[0]
+    puts array[0]
   elsif array.size == 2
-      array.join(' and ')
+      puts array.join(' and ')
   elsif array.size > 2
     last_item = array.pop
     fruit_list = array.join(', ')
     fruit_list << ", and #{last_item}"
     array.push(last_item)
-    return fruit_list
+    puts fruit_list
   end
 end
+
+### learn's solution
+#def oxford_comma2(array)
+#  if array.length == 2
+#    return "#{array[0]} and #{array[1]}"
+#  elsif 2 < array.length
+#    array[-1].insert(0, "and ")
+#  end
+#  array.join(", ")
+#  binding.pry
+#end
