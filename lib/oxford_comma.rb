0da4e366cf2f22ee976ccfd3d4b077d739ab4343
tyleresmith <oats000@hotmail.com>
2016-02-09 23:42:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,13 @@
 def oxford_comma(array)
-  array.join(", ")
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 1
+    a_last = array.pop
+    a_string = array.join(", ")
+    comboString = "#{a_string}, and #{a_last}"
+  else
+    array.join
+  end
 end
 
 test=["The blue cat", "red bird", "green duck"]
