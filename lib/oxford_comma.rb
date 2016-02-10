9a338c0b6a3dfdea43240899b927559bdebce273
marcelgalang <marcelgalang@gmail.com>
2016-02-10 13:18:19 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+     array.join(" and ")
+  elsif array.size >= 3
 
+   array.insert(-2, "and ")
+    new_array= []
+    array.each do |word|
+      new_array.push word
+    end
+    new_array.each do |word|
+      word + ", "
+    end
+    new_array[0..-2].join(", ") + new_array[-1]
+  end
 end
