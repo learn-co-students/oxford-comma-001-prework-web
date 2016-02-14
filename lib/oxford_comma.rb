f524595a6a5e4561f3eac761dfd4cab8243a32b5
Cliff <cregisexcel@yahoo.com>
2016-02-14 14:27:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if  array.length > 2
+    newarray = array.insert(-2, "and ")
+    array = newarray.join(", ")
+    return array.gsub(/and ,/, "and")
+  elsif array.length == 2
+    return array.join(" and ")
+  end
+  return array.join
 end
