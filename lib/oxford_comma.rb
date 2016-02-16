24acf59b425e649b86377c504d3ea673530dc19b
blochbr <blochbr@gmail.com>
2016-02-16 16:37:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+      array.join
+    when 2
+      array.join(" and ")
+    else
+      array[-1..-2] = "and"
+      array.join(", ").sub('and,', 'and')
+  end
 end
+
