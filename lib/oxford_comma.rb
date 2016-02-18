380666756e4936a5fda51f32ab2f75f55105bcb2
kazushige Miyao <yura.soverigns@gmail.com>
2016-02-18 13:31:35 +0900

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,37 @@
+=begin
 def oxford_comma(array)
+	message = ""
+	if array.size == 1
+		message = "#{array[0]}"
+	elsif array.size == 2
+		message = "#{array[0]} and #{array[1]}"
+	elsif array.size == 3
+		message = "#{arrray[0]}\, #{array[1}\, and #{array[2]}"
+	message
+end
+
+=end
+require 'pry'
+def oxford_comma(array)
+	message_over_3 = []
+	if array.size == 1
+		message = array.shift
+	elsif array.size == 2
+		message = array.join(" and ")
+	elsif array.size >= 3
+		counter = array.size - 2
+		for test in 0...array.size #this sentence is not smart , I will refactor soon
+			if test < counter
+				message_over_3.push(array[test], ", ")
+			elsif test == counter
+				message_over_3.push(array[test], ", and ") 
+			else 
+				message_over_3.push(array[test]) 
+			end
+		end
+		message = message_over_3.join("")
+		#
+	end
+	message
 
 end
