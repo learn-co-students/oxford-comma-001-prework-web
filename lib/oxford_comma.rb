ed9cf8d0b20e24252f3bb79c37a0dbc4a7ad7d1e
jasonkwong11 <jasonkwong11@gmail.com>
2016-02-24 18:10:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
+if array.length == 1
+array.join
+  elsif
+  array.length == 2
+  array.join(" and ")
+  elsif
+  array.length == 3
+  aaa = "and "
+  last = array.pop
+  last.insert(0,aaa)
+  array.join(", ")
+  array << last
+  array.join(", ")
+  else
+last = array.pop
+string_to_add_to_last = ", and "
+  string_to_add_to_last << last
+new = array.join(", ")
+  new << string_to_add_to_last
+end
 
 end
