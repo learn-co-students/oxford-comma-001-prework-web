3ed030140d5ae7f1d27224d34f687fa8af7c8f6f
supermancbs <clark.sacktor@gmail.com>
2016-02-24 22:50:56 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+	if array.size<=1
+		return array.join
+		
+	elsif array.size==2
+		return array.join(" and ")
+	else
+		
+		x = "and " + array.last 
+		array.pop 
+		array.push x 
+		return array.join(", ")
+		
+		
+	
+	end  
+	
 end
