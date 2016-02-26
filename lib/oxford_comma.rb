909556407a71a2cdaa12d284e62c4b434cda0b62
gnwankwo <gnwankwo03@gmail.com>
2016-02-26 14:35:57 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size < 3
+    array.join(' and ')
+  elsif array.size >= 3
+    last=array.pop
+    sentence= 'and ' + last
+    array.push(sentence)
+    array.join(', ')
+  end
 end
