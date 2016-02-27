3567acf881977a3422833999a5d48c771878ae48
czarnega <c.zarnegar@gmail.com>
2016-02-27 14:32:30 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+  str = ""
+  tempArr = []
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    str = array[2]
+    str.insert(0,"and ")
+    array[2] = str
+    array.join(", ")
+  else
+    num = array.length - 1
+    str = array[num]
+    str.insert(0,"and ")
+    array[num] = str
+    array.join(", ")
+  end
 
+      
 end
