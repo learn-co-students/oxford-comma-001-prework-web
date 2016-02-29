58b06c941fc659cf7e36bd1d9069ef38c143741d
Rayshawn Woolard <r.woolard456@gmail.com>
2016-02-29 19:05:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2 then
+     return array.join(" and ")
+    elsif array.length == 1 then
+      return array.join
+      else
+       array[-1] = "and " + array[-1]
+       array.join(", ")
+  end
 end
+
