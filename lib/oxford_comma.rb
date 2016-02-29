c5460015c53c6245a5fb43e046c2ec355853ca72
akizzles <aykim002@gmail.com>
2016-02-28 23:17:57 -0500

    done/

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last = array.pop
+    comma = array.join(", ")
+    comma_last = comma + ", and " + last
+  end
 end
