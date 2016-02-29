2134a82675936b15b42668f3bc7019ad21becc87
snatow <natow.sarah@gmail.com>
2016-02-28 19:44:05 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    string = array.join
+    string
+  elsif array.length == 2
+    string = array.join(" and ")
+    string
+  else
+    string = array[0...-1].join(", ") + ", and " + array[-1]
+  end
+      
 end
