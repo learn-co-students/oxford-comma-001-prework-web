e51354730b1cc130e6134c4b693d1af3ad0880c7
kiddeath91 <jason@laskodi.com>
2016-03-01 11:38:22 -0500

    Oxford Comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array[0].to_s
+  elsif array.count == 2
+    array.join(' and ')
+  else array[-1] = "and " + array[-1]
+    array.join (", ")
+  end
 end
