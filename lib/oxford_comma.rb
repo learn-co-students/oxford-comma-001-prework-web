aea813292c2cf4a3a464485467f1529af1e0ecb5
hdwatts <howard@hdwatts.com>
2016-03-01 10:20:58 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  string = "";
 
+  array.each_index do |x|
+    string += "#{array[x]}"
+
+    if ( x == array.size - 2 )
+      if ( array.size >= 3 )
+        string += ", and "
+      else
+        string += " and "
+      end
+    elsif ( x != array.size - 1 )
+      string += ", "
+    end
+  end
+
+  return string
 end
