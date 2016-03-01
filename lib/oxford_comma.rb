ecd5aba0039a74b03d94ab749573190cc9343d52
Chad Baum <chadbaum@gmail.com>
2016-03-01 15:27:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
 
+    string = ""
+
+    array.each_with_index do |x, index|
+
+      if index == 0
+        string << "#{x}"
+      elsif array.length == 2 && index == 1
+        string << " and #{x}"
+      elsif index == array.length - 1
+        string << ", and #{x}"
+      else
+        string << ", #{x}"
+      end
+
+    end
+
+    return string
 end
