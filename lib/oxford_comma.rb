ec50f0284a16ac501fb3950207672487bb033ec7
robintully <mr.robintully@gmail.com>
2016-03-04 11:53:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+ line = ""
+ final = "and "
+  if array.length == 1
+   return array.join
+  end
 
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+    
+  else
+    final << array.last
+    array.pop
+    array. each do |entry|
+    line << "#{entry}, "
+  end
+end
+line << final
 end
