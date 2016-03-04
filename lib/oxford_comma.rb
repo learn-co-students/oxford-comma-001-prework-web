45ff5a54c21e54ee3fda17f0e5f0ad6a27ef449e
jasmin47 <alxsanborn@gmail.com>
2016-03-04 14:03:21 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array[0] + " and " + array[1]
+  else 
+      array_2 = []
+    i = 0
+    while i < array.length - 1
+      array_2 << array[i] + ", "
+      i += 1
+    end
+    return array_2.join + "and " + array[-1] 
+  end
 end
