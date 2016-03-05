3a133c8b7fab8c99275bb85a53e79be4361c9d22
asackofwheat <asackofwheat@gmail.com>
2016-03-05 00:06:40 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 0
+    return array
+  elsif array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.size >= 3
+    last_string = array.pop
+    return "#{array.join(", ")}, and #{last_string}"
+  end
 end
