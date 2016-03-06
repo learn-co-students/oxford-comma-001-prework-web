da2d7ebc8dfceafd20f062c18c220c311db9c365
nghuman <navkiran.ghuman@gmail.com>
2016-03-06 20:56:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length > 2
+    array.last.prepend("and ")
+    array.join(", ")
+  else
+    array.join(" and ")
+  end
 
 end
