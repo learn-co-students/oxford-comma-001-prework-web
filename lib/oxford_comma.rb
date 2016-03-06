232f1e93b4d44253db02cd3a084859634398b5bb
LibraHampton <LibraHampton@live.com>
2016-03-06 03:13:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  case array.size
+    when 1
+    array.first
+    when 2
+    array.join(" and ")
+    else
+    "#{array[0..-2].join(", ")}, and #{array.last}"
 
+
+  end
 end
