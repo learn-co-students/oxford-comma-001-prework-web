bbd6d082f2cb40fd57d67841d26e4d76f73868a4
abillig <abermania@gmail.com>
2016-03-09 15:13:35 -0500

    Done.

--- lib/testing.rb
+++ lib/testing.rb
@@ -6,7 +6,9 @@ elsif array.length == 2
 else 
   testing = array[array.length-1]
   array.pop(1)
-  joined = array.join(", ")
-  return joined << ", and #{testing}"
+  joined = array.join(",")
+  puts joined << ", and #{testing}"
 end
 end
+
+oxford_comma(["one", "two", "three", "four"])
