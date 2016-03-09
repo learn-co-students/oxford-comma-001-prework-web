831e0982e438dede4df7f6db83094090091a515c
Sebastian <shimomichi63@gmail.com>
2016-03-09 08:02:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.length
+    when 0
+    when 1
+      array.join
+    when 2
+    array.join(" and ")
+    else
+    final = array.pop
+    array.join(", ") << ", and " << final
+  end
 end
