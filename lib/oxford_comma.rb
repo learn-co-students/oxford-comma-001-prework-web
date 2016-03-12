554f9467b0488241ddda08352a02e22d800b18f8
KevinDTran <kevin@zipmark.com>
2016-03-12 01:57:19 -0600

    Completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join (' and ')
+  elsif array.count == 2
+    array.join (' and ')
+  elsif array.count == 3
+    first_two = array.slice(0,2)
+    last = array.slice(2)
+    list1 = first_two.join (', ')
+    array = list1<< ", and #{last}"
+  else 
+    max = array.count
+    not_last = array.slice(0,max-1)
+    last = array.slice(max-1)
+    list1 = not_last.join (', ')
+    array = list1<< ", and #{last}"
+  end
 end
+   
