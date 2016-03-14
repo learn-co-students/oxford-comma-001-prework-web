66712805b12e2b4aa3c21cb333c31916acf526f6
jasmin47 <alxsanborn@gmail.com>
2016-03-14 12:44:15 -0400

    Update oxford_comma.rb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return array.join
   elsif array.length == 2
-    return array[0] + " and " + array[1]
+    return array.join(" and ")
   else 
       array_2 = []
     i = 0
