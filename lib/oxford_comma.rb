510350631578cbbe078c58cc4eac23f692af022a
miggyteez <mjtavarez@yahoo.com>
2016-03-14 17:32:15 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  formatting_conditions = array.size
 
+  if formatting_conditions == 1
+    array.join
+  elsif formatting_conditions == 2
+    array.join(" and ")
+  else
+    last_element = array.pop
+    oxford_string = array.join(", ")
+    oxford_string << ", and #{last_element}"
+    oxford_string
+  end
 end
