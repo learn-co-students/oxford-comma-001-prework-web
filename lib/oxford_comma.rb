1c440bfc1c541ed6d7a40d9643a157306806176d
erinpettigrew <erin.pettigrew@gmail.com>
2016-03-16 12:42:48 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array[-1] = " and " + array.last
+    array.join("")
+  elsif array.length > 2
+    array[-1] = "and " + array.last
+    array.join(", ")
+  else
+    array.join
+  end
 end
