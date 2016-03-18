b8bdc1732a554a74f2581afef6badef6dd39073e
drewvollrath <drew@vollrath.ca>
2016-03-18 21:37:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  size = array.size()
+  if size==2
+    array.first << " and "
+  elsif size>2
+    for k in 0..size-2
+      array[k] += k<size-2 ? ", " : ", and "
+    end
+  end
+  array.join
 end
+
