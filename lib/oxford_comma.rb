28b78ae096dd2a3b0dba9ae613a65707fee1e225
WerkerM <michael.handwerker@gmail.com>
2016-03-20 18:41:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+  if array.length < 3
+    array.join(" and ")
+  else
+    array.collect! { |last| (last == array.last) ? "and #{array.last}" : last }
+    array.join(", ")
+  end
 end
