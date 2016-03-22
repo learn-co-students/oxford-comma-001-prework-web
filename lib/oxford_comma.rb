1e3409fad45a93ab48e059271f51bebad04ea989
shalin1 <shalin.scupham@gmail.com>
2016-03-22 02:21:33 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return array.join(" and ")
+  elsif array.length > 2
+  	#array[-1] is a string, 
+  	#so #insert is acting as a string method 
+  	#appending "and " to the start of the last string in the array
+    array[-1].insert(0, "and ")
+  end
+  return array.join(", ")    
 end
