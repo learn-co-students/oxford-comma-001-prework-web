9e991b83222a15462ceec628bd020f73311ce3f1
drewvollrath <drew@vollrath.ca>
2016-03-23 22:49:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
-def oxford_comma(array)
+##Original Code
+def xoxford_comma(array)
   size = array.size()
-  if size==2
+  if size==2 
     array.first << " and "
   elsif size>2
     for k in 0..size-2
@@ -10,3 +11,13 @@ def oxford_comma(array)
   array.join
 end
 
+#Rewritten with Enumerators
+def oxford_comma(array)
+  arr_ele = array.pop
+
+  if array.length > 1 then array.collect {|x| x << ","} end
+  if array.length > 0 then array[-1] << " and" end
+
+  array << arr_ele
+  array.join(" ")
+end
