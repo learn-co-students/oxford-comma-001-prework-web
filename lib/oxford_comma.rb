bf8471cd7404c471d57f10a7f333e71858d0dd43
sheafk <sheafureyking@gmail.com>
2016-03-29 02:57:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    new_string = array.join
+    new_string
+    elsif array.length == 2
+    array.join(" and ")
+    elsif array.length >= 3
+    last = array.pop
+    array.join(", ") + ", and " + last.to_s
+  end
 end
