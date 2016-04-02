2b112ed15fa0174811ab46ef0233c48ba2a13ba2
ToddRizley <todd.rizley@gmail.com>
2016-04-02 01:49:33 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.length == 1
+    array[0].to_s
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    string = String.new
+    array.each_with_index do |element, index|
+      index < array.length - 1 ? string << "#{element}, " : string << "and #{element}"
+    end
+    string
+  end
 end
+
+
