388183a3479c77b86750a56a89543ab36b744623
mitchwd <mitch@mitchwd.com>
2016-04-19 22:51:06 +0930

    Refactored.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,8 @@
 def oxford_comma(array)
-  if array.size == 1
-    array.first
-  elsif array.size == 2
-    array.join(" and ")
-  elsif array.size == 3
-    array[0] + ", " + array[1] + ", and " + array[2]
-  elsif array.size > 3
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
     array[-1].insert(0, "and ")
-    array.join(", ")
   end
-
+  array.join(", ")
 end
