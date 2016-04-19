2c1b7634495a46549fa14fd3ca6431589e9b0449
mitchwd <mitch@mitchwd.com>
2016-04-19 22:49:06 +0930

    Working, needs refactoring

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.size == 1
+    array.first
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    array[0] + ", " + array[1] + ", and " + array[2]
+  elsif array.size > 3
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  end
 
 end
