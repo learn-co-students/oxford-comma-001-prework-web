cd223f667899860b2c7f9152cdd112123acab877
samanthabertous <sam@samanthabertous.com>
2016-04-20 15:43:54 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  #array.join
+  if array.length == 2
+  	array.insert(1," and ").join
+  elsif array.length ==1
+  		array.join
+  else array.length >2
+  	last = " #{array.pop}"
+  	array << "and"
+  	array.join(", ") << last
+  	
 
+  end
 end
