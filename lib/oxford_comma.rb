8b4d36e11b2563a7b20d1a1e2c3c5cfeb2b32b2b
wvazquez <vazquezwilmer@gmail.com>
2016-04-21 13:09:40 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  new_array = array.join(", ")
+  if array.length == 2
+      new_array = new_array.sub(",", " and")
+  elsif array.length > 2
+        new_array = new_array.reverse.sub("," , ", and".reverse).reverse
+    end
+    return new_array
 end
