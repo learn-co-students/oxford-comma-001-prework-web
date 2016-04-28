1b9907a0f7d389ca1a32aaba96ca8c8594d21849
chugachski <natesmith.copper@gmail.com>
2016-04-28 15:25:35 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,20 @@
 def oxford_comma(array)
-  if array.length == 2
+  if array.count == 2
     "#{array[0]} and #{array[1]}"
-  elsif array.length > 2
-    array[-1].insert(0, "and ")
-    array.join(", ")
+  elsif array.count > 2
+    # => ["chorizo", "capicola", "pastrami"]
+    array.insert(-2, "and")
+    # => ["chorizo", "capicola", "and", "pastrami"]
+    last = array.pop(2).join(" ")
+    # => ["and", "pastrami"]
+    # => and pastrami
+    first = array.join(", ")
+    # => "chorizo, capicola"
+    first << ","
+    # => "chorizo, capicola,"
+    "#{first} #{last}"
+    # => "chorizo, capicola, and pastrami"
   else
     "#{array[0]}"
   end
-   
 end
