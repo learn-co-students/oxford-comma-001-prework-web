3d05e0191bf0d5e780e940b1fb28b4dac5af756d
dhechagas <dhechagas@gmail.com>
2016-04-29 17:24:39 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
 
+  if array.size == 1 then
+    return array.first
+  elsif array.size == 2 then
+    return array.join(" and ")
+  end
+
+  last_element = array.pop()
+
+  ret = array.join(', ')
+
+  ret << ", and " + last_element 
+
+  return ret
 end
