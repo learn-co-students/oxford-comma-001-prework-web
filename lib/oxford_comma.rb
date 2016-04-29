c89a43b10e3eb0b7a046ba7657229ed93ac6a833
greatermeans <gakouri@gmail.com>
2016-04-29 00:04:12 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length > 1
+      array[-1] = "and " + array[-1]
+  end
+  if array.length < 3
+      array.join(" ")
+  else
+      array.join(", ")
+  end
 end
