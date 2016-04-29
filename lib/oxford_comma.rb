918a248c93072d92eb257ff66c85e944cabe6868
p1xt <kjphpdev@gmail.com>
2016-04-29 20:52:07 +0000

    implemented oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1 || array.size == 2
+    array.join(" and ")
+  else
+    final = array.pop
+    output = array.join(", ")
+    output += ", and #{final}"
+    output
+  end
 end
