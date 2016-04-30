b546fa7db1c3ba081e5a0d69f5c619dc567cab66
Max E <etiemaxo@gmail.com>
2016-04-30 18:36:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+      return array.join
+    elsif array.size == 2
+      return array.join(" and ")
+    else
+      convert = array.last
+      array.pop
+      string = array.join(", ")
+      string << ", and #{convert}"
+    end
 end
