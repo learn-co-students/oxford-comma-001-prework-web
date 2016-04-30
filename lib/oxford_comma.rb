3d2f255996710ff4dc2df5645b473d3b61ae9a66
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-04-30 23:42:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if (array.length == 1 )
+    string = array[0].to_s
+    return string
+  elsif (array.length == 2)
+    string = array[0] + " and " + array[1]
+  else
+    array[-1] = "and " + array[-1]
+    string = array.join(", ")
+    return string
+  end
 end
