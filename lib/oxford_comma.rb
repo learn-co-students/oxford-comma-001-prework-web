04b366a470e533669db796e685eddcaece9551a3
bshap93 <benihana858@gmail.com>
2016-05-03 15:42:30 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  last = array.pop
+  first_few = array.join(", ")
+  if array.size == 0
+    "#{last}"
+  elsif array.size == 1
+    "#{array.join} and #{last}"
+  else
+    "#{first_few}, and #{last}"
+  end
 end
