c45128deefa4230931550f16b1d872b7e7d7f99d
ivanyyuk <ivanyyuk@gmail.com>
2016-05-07 18:30:16 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+str =""
+  if array.size == 1 
+    str = array.join
+  elsif array.size==2
+    str = array.join(" and ")
+  else
+    array.each_with_index do |name, index|
+      if index == array.size-1
+        str += "and #{name}"
+      else
+      str += "#{name}, "
+    end
+  end
+end
+  str
 end
