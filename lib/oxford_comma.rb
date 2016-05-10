68e7eb187213c2840c06dedb662be820cb6e2a09
hailuhr <hredleaf@gmail.com>
2016-05-10 02:36:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  return nil if array.empty?
+  return array[0] if array.length == 1
+  if array.length > 2
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  else
+    return array.join(' and ')
+  end
 
+  #return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1
 end
