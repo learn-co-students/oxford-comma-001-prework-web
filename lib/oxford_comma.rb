3c6b15ddacf0d366983b57ed59ed0ae23e6c2ce8
Daniel Caligiuri <daniel.caligiuri44@gmail.com>
2016-05-13 20:54:41 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.length == 1
+  	b = array.join
+  elsif array.length == 2
+    b =array.join(' and ')
+  elsif array.length >= 3
+  	a = array.pop
+  	b = array.join(', ')
+  	b << ", and #{a}"
 
+  end
+  return b
 end
