4cb5c405b382634e19472046dcb7908e78aa4e6a
Salome Braganza <salomekbg@gmail.com>
2016-05-16 18:18:39 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  string = ""
+  index = 0
+  if array.length == 1
+    string = "#{array[index]}"
+  elsif array.length == 2
+    string = array.join(" and ")
+  else
+    while index < array.length
+      if index == (array.length - 1)
+        string << "and #{array[index]}"
+        break
+      else
+        string << "#{array[index]}, "
+        index += 1
+      end
+    end
+  end
+  string
 end
