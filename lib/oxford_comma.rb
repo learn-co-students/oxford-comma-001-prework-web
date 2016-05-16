1b642ebe55c89767a82d4fc9a91309196d2db096
Salome Braganza <salomekbg@gmail.com>
2016-05-16 18:36:01 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,4 @@
+
 def oxford_comma(array)
   string = ""
   index = 0
@@ -18,3 +19,14 @@ def oxford_comma(array)
   end
   string
 end
+
+=begin
+def oxford_comma(array)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
+end
+=end
