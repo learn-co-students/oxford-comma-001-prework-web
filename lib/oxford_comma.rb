60ddd72a7cadfb495ddfb2790542b0e202e8d855
conuska <onuskacc@gmail.com>
2016-05-20 01:22:37 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(' and ')
+  else
+    [array[0...-1].join(", "), array.last].join(", and ")
+  #   array.join(','last_comma = false ' and ')
+  #   # return "#{array.join(', ')} and #{array.last}"
+    # array.to_sentence(last_word_connector: ' and ')
+    # array.join
+      # array.to_sentence
+    # array.join(', ')+(' and ')
+  end
 end
