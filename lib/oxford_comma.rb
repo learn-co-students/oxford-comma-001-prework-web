703325ec96e6df9e333d799de9b243c9f33e52e4
Ece Ozalp <ozale272@newschool.edu>
2016-05-23 14:04:15 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    last_element = array.last
+    array.pop
+    almost_formatted = array.join(", ")
+    to_be_joined = ", and #{last_element}"
+    almost_formatted << to_be_joined
+  end
 end
