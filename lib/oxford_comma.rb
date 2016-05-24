8b5c6a365c79a4e4bfefb42ae23e2119f18da790
Salome Braganza <salomekbg@gmail.com>
2016-05-24 17:22:14 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,32 +1,26 @@
-
-def oxford_comma(array)
-  string = ""
-  index = 0
-  if array.length == 1
-    string = "#{array[index]}"
-  elsif array.length == 2
-    string = array.join(" and ")
-  else
-    while index < array.length
-      if index == (array.length - 1)
-        string << "and #{array[index]}"
-        break
-      else
-        string << "#{array[index]}, "
-        index += 1
-      end
-    end
-  end
-  string
-end
-
 =begin
 def oxford_comma(array)
   if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif array.length > 2
+    return "#{array[0]} and #{array[1]}" #since the return is stated here, this will return last, not the last statement in the code!    
+  elsif 2 < array.length
     array[-1].insert(0, "and ")
   end
   array.join(", ")
 end
 =end
+
+def oxford_comma(array)
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    new_string = ""
+    i = 0
+    while i < (array.length - 1)
+      new_string << "#{array[i]}, "
+      i += 1
+    end
+    new_string + "and #{array[-1]}"
+  end
+end
