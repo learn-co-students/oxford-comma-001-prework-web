5dcca70e69d09db9e44c61a3ddb1b7aa693a23a4
Lee Araneta <lee.araneta@gmail.com>
2016-05-23 20:18:15 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    x = array.pop
+    z = x
+  elsif array.size == 2
+    z = array.join(" and ")
+  else
+    x = array.pop
+    y = array.join(", ")
+    z = y + ", and " + x
+  end
+  z
 end
+
+oxford_comma(["kiwi", "strawberry"])
