2bd3b676b1d54fc5a765fa141842d1c5e633d4f2
Charlie Cook <turingbombe@gmail.com>
2016-05-24 20:25:10 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+  list = ""
 
+  if array.length == 1
+    list = array[0]
+  elsif array.length == 2
+      num = array[1]
+      num = (num.length) +1
+      list = array.join
+      list.insert(-num, " and ")
+      list
+  else
+      x = (array.size) - 1
+      num = array[x]
+      num = (num.length) +1
+      
+      list = array.join(", ")
+      list.insert(-num, "and ")
+      list
+  end
 end
