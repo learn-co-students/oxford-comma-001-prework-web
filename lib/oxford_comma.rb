f1e2ba10e37eec9df9476f86f878d6a4fa7ce37e
barbiecode <helesa@gmail.com>
2016-05-25 10:10:39 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    if array.count == 1
+        return array.first
+        else if array.count == 2
+        last_two_elements = array.join(" and ")
+        return last_two_elements
+        else
+        last_element = array.last
+        array.pop
+        new_string = array.join(", ")
+        return "#{new_string}, and #{last_element}"
+        end
+end
 end
