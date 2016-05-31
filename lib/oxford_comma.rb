1149f5eace8545475d71b7ac9bf883569f795500
Khaleda Khan <khaleda.khan87@gmail.com>
2016-05-30 23:33:18 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    first = array.slice(0, array.length-1).push("and").join(', ')
+    return first.concat(" " + array.slice(array.length - 1))
+  end
 end
