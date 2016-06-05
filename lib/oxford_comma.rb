12c69273fb44f3263a16affaf6a740992fc07e0c
jinstrider2000 <efrainperezjr@live.com>
2016-06-05 02:19:17 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    list = "#{array.shift}, "
+    while array.length > 1 
+      list << "#{array.shift}, "
+    end
+    list << "and #{array.shift}"
+  end
 end
