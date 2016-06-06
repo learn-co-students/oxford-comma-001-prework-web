3dd50930897a75c63aaf20101b33c154a81c1eef
abidiaz <abidiaz@users.noreply.github.com>
2016-06-06 14:11:38 -0400

    Update oxford_comma.rb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
-#[array].join(",")
 
   if array.size == 1
-    [array].join
+    array.join
   elsif array.size == 2
-    [array].join(" and ")
+    array.join(" and ")
   elsif array.size >= 3
     return string = array[0..array.size-2].join(", ") + ", and " + array.last
   end
