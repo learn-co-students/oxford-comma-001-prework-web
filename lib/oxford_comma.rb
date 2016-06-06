cdedf843cd059e1ee634ee317d19a53b575736ee
abidiaz <abigaildiaz@gmail.com>
2016-06-06 02:39:55 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+#[array].join(",")
+
+  if array.size == 1
+    [array].join
+  elsif array.size == 2
+    [array].join(" and ")
+  elsif array.size >= 3
+    return string = array[0..array.size-2].join(", ") + ", and " + array.last
+  end
+
+
 
 end
