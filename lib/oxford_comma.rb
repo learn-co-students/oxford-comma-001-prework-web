f50285eea0fdc2b808fe96dc488a459df2d9ba0d
Tami <catquarks@gmail.com>
2016-06-07 22:54:34 -0400

    Added Oxford commas.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+	if array.size == 2
+		string = array.join(" and ")
+	elsif array.size > 2
+		last_item = array.pop
+		string = array.join(", ")
+		string = string.insert(-1, ", and " + last_item)
+	else
+		string = array[0]
+	end
 end
