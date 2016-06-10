60f35097304a79daaeb836bc1d66c9b9aae36f50
KSauri <kylesauri@gmail.com>
2016-06-10 08:45:27 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    arLength = array.length
+    if arLength == 0 || arLength == 1
+        return array.join
+    elsif arLength == 2
+        return "#{array[0]} and #{array[1]}"
+    else
+        shortArray = array[0..(arLength-2)]
+        strWithCommas = shortArray.join(", ")
+        finalStr = strWithCommas += ", and #{array[-1]}"
+        finalStr
+    end
 end
