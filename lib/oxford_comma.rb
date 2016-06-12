3ee042525612ac4917262c31442a1befa589a090
Hafizur Rahman <hafizur.rahmanc@gmail.com>
2016-06-12 21:12:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    temp = "and " + array.last
+    array[-1] = temp
+    return array.join(", ")
+  end
+
 
 end
