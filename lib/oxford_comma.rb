0cf1e9fadbf89695c2871c547a8bcef33b9b182a
Silvana Goberdhan-Vigle <silvanagoberdhanvigle@gmail.com>
2016-06-12 13:10:17 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    x = 0
+    string = ""
+    array_length = array.length
+    while x < (array.length - 1)
+      string += "#{array[x]}, " 
+      x += 1
+    end
+    string += "and #{array[x]}"
+  end
+    
 end
+
