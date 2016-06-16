02fa62b806125130113f89422b16a83fbc60c9cd
lshapz <likeaduck@gmail.com>
2016-06-16 15:17:29 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  
+if array.length == 1
+array.join(" ") 
+elsif array.length == 2
+array.join(" and ")
+else
+  poop = []
+  poop << array.pop 
+  string = array.join(", ") 
+  poop.unshift(", and ")
+  string = string + poop.join
+end
 end
