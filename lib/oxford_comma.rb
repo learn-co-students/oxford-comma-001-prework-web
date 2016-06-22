1987ae2ef679388964d000fcb9ffb0184df5c79d
ivanjanko <jankoemail@gmail.com>
2016-06-22 18:19:40 -0400

    oxford_comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.count == 1
+      array.join
+  elsif array.count == 2
+        array.join(" and ")
+  elsif array.count == 3
+     new_string = "and #{array.pop}"
+     array << new_string
+      array.join(", ")
+  else array.count > 3
+      new_string = "and #{array.pop}"
+     array << new_string
+      array.join(", ")
+  end
 end
