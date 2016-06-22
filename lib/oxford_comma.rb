c4c0edc1f61888929bd63a2f78acdd893c501ea3
Dillon Powers <dillon.c.powers@gmail.com>
2016-06-22 08:00:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  array_size = array.size
+  if array_size == 1
+    array.join
+  elsif array_size == 2
+    array.join(" and ")
+  else
+    ending = ", and "
+    ending << array.pop
+    array.join(", ") << ending
+  end
 end
