74c4852aec2dc891a5f249375725199b5cf11579
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-06-23 13:43:35 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  last_word = array.pop 
+  if array.length > 1
+    first_part = array.join(", ")
+    final_phrase = "#{first_part}, and #{last_word}"
+  elsif array.length == 1
+    final_phrase = "#{array[0]} and #{last_word}"
+  else final_phrase ="#{last_word}"
+  end
 end
