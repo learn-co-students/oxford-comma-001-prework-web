fe4013cdc264c7a806fcf0cdd2f2ba362a8f4e87
samaugust <sam.august@gmail.com>
2016-06-24 00:26:56 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-
-end
+  return array[0] if array.length == 1
+  return array.join(" and ") if array.length == 2
+  final = array.pop
+  array = array.join(", ")
+  return array.concat(", and #{final}")
+end  
