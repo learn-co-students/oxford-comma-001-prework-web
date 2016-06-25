4a01e28cd0a60b02466e62c778ed1d8295b87fac
Isuru Vidanapathirana <isuru260@gmail.com>
2016-06-25 07:12:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
+
 def oxford_comma(array)
 
+if (array.size ==1)
+  completeWord = array[0]
+   completeWord
+elsif (array.size ==2)
+  completeWord = array.join(" and ")
+  completeWord
+elsif(array.size >= 3)
+  newArray = Array.new
+  newArray = array.pop
+completeWord = array.join(", ")
+ completeWord << ", and "
+ completeWord << newArray
+  completeWord
+
+
+end
 end
