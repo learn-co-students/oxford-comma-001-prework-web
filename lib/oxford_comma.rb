327f60825dc463c4d67df1c19a52a7bd8d6e21f3
Satu Barnhill <satu.barnhill@gmail.com>
2016-06-27 19:38:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    string = array.join(" and ")
+  elsif array.length <= 2
+    string = array.join
+  else
+   string = ""
+   punctuation = Array.new(array.length-1, ", ")
+   punctuation[-1] = ", and "
+   punctuation << ""
+   array.each_with_index do |element, index|
+     string = string + "#{element}" + "#{punctuation[index]}"
+   end
+  end
+  string
 end
