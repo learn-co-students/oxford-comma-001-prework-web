551200981f33d946ce4f194707c2284b3c459e2c
John Molina <jrmolin90@gmail.com>
2016-06-27 17:05:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
 
+  if (array.size == 1)
+    completeword = array[0]
+    completeword
+    elsif (array.size == 2)
+      completeword = array.join(" and ")
+      completeword
+    elsif(array.size >= 3)
+      newArray = Array.new
+      newArray = array.pop
+    completeword = array.join(", ")
+    completeword << ", and "
+    completeword << newArray
+    completeword
+  end
 end
