7043cc580bd8920731ce735ca99ea97e11e09506
FJ Collins Jr <fcollins@live.com>
2016-06-29 19:00:27 -0400

    refactor solution

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,7 @@
 def oxford_comma(array)
-    if array.length == 1
-        array.join
-    elsif array.length == 2
+    if array.length == 2
         return "#{array.first} and #{array.last}"
-    else
+    elsif array.length > 2
         array[-1].insert(0, "and ")
     end
     array.join(", ")
