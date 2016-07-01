212d240dce75a11c748c31d7a99f34e440fd59fe
mo6709 <mo6709@gmail.com>
2016-07-01 16:52:03 -0400

    short code

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,34 @@
 def oxford_comma(array)
 
  case array.length 
+ 	
   when 1
+  #return string with the same element with no oparation on it.
     "#{array[0]}"
   when 2
+  #return string with "and" between the two elements 
     array[0..1].join(" and ")
   else 
+  	#return string appanded with "," between all the elements -1 element from the end of the array 
+  	#appand "and" to the last element of the array
     array[0...-1].join(", ") << ", and #{array[-1]}"
   end 
 	   
 end
 
- 
+#lines of code that can be expressed in very small chunk of code!
+ 	# if array.length == 1
+	# 	array.join
+	# elsif array.length == 2
+ #      array.insert(1 , " and ")
+ #      array.join
+	# elsif array.length == 3
+	#    array.insert(1 , ", ")
+	#    array.insert(3 , ", ")
+	#    array.insert(-2 , "and ")
+ #       array.join
+	# else
+	# 	array.join(", ")
+	# 	array.insert(-2, "and ")
+		
+	# end
