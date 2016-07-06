2e476e0f8d51115142739c019ea3e9c99d2bf8d5
Esmery Corniel <esmerycornielle@gmail.com>
2016-07-06 11:29:52 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def oxford_comma(array)
-
+  length = array.length - 1
+  string = ""
+  if array.empty? || array.length == 1
+    array.join
+  elsif array.length==2
+    array.join(" and ")
+  else
+    array.each do |var|
+    array.index(var) != length ? string<<"#{var}, " : string<<"and #{var}"
+    # binding.pry
+    end
+    string
+  end
 end
