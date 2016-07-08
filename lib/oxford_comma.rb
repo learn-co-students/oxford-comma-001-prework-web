30176967fb04224acba20cf208e487a0aa7be92c
SharkBroPhD <troychryssos@gmail.com>
2016-07-08 15:17:57 -0400

    Wrote method 'oxford comma' that would break arrays into lists of items.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  new_string=""
+  if array.count<=1 
+    new_string<<array[0].to_s
+  elsif array.count==2
+    new_string<<array.join(" and ")
+  elsif array.count > 2
+    array.each { |item|
+      if item == array.last
+        new_string<<"and #{item}"
+      else
+        new_string<<item+", "
+      end
+    }
+  end
+  new_string
 end
