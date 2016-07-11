fe6ef23f282e7ca70c42fcdb6d07c68e5a360a51
Jason Arnold <jason.arnold.00@gmail.com>
2016-07-11 20:32:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,16 @@ def oxford_comma(array)
     return array[0]
   elsif array.count == 2
     array.join(' and ')
+  elsif array.count == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.count > 3
+    counter = 0
+    str = ''
+    while counter < array.count - 1
+      str += array[counter] << ', '
+      counter += 1
+    end
+    str += "and #{array[-1]}"
+    return str
   end
 end
