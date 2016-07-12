ce6f5445ad53bdd0dd06030037ff148c32403bd9
Jacob Edgerly-Dowd <dowdje89@gmail.com>
2016-07-12 01:25:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  new_sentence = ""
+  if array.length == 1
+    new_sentence = array[0]
+  elsif array.length < 3
+    new_sentence = "#{array[0]} and #{array[1]}"
+  else
+    for spot in 0..array.length - 2
+      new_sentence << "#{array[spot]}, "
+    end
+    new_sentence << "and #{array[-1]}"
+  end
+new_sentence
 end
