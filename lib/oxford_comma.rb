8430fb503c0ebf91f9163ff6dc8af40c31e0b10c
Colby Gatte <colbygatte@gmail.com>
2016-07-13 21:07:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(' and ')
+  else
+    last_item = ", and " + array.pop
+    sentence = array.join(', ') + last_item
+    return sentence
+  end
 end
