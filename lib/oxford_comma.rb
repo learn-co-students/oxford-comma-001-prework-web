17a5b7d4744906e05922510809c9646365ba30ce
Janelle <janelle.rosario@gmail.com>
2016-07-14 14:15:17 -0400

    Delete oxford_comma.rb

--- lib/oxford_comma.rb
+++
@@ -1,9 +0,0 @@
-def oxford_comma(array)
-  if array.size == 1
-    array.first
-  elsif array.size == 2
-    array.join( " and " )
-  else
-    array[0..-2].join(", ") << ", and " << array.last
-end
-end
