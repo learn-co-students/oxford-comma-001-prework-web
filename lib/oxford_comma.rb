bb182500c017a794723a75b785ac46f4c4ce8caf
OliviaGunton <olivia.gunton@gmail.com>
2016-07-17 17:30:39 -0400

    Join strings using Oxford comma rules

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+  len = array.count
 
+  #Special cases
+  if len <= 1 then
+    array.pop()
+  elsif len == 2 then
+    array[0] + " and " + array[1]
+  else
+    #Iterate through all but the last one, appending ", "
+    #Add last item prepended with "and "
+    output = ""
+    last = array.pop()
+
+    for string in array do
+      output += string + ", "
+    end
+
+    output += "and " + last
+  end
 end
