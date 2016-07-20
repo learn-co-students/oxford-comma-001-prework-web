c103497838763bec6ff294b120f5f9dc31ca6a09
Charlie Spencer <crs992@gmail.com>
2016-07-20 20:08:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  num = array.size
+  sing = array[0]
+  final = num - 1
 
+  if num == 1
+    array[0]
+  elsif num == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    ans = "#{array[0, final].join(", ")}, and #{array[final]}"
+    ans
+  end
 end
