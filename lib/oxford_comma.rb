11dbe5ab28f7fd3bab33295d14a78a82fd9b755a
Marc Immediato <marc@beldgroup.com>
2016-07-24 11:22:28 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  result = array.join("") if array.size == 1
+  result = array.join(" and ") if array.size == 2
+  if array.size > 2
+    last = array.pop
+    array.push("and "+last)
+    result = array.join(", ")
+  end
+  result
 end
