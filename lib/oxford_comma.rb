b9bbfb00311db1da15778229396c8c3399715e1b
Steven Tang <5teven7ang@gmail.com>
2016-07-27 22:10:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  counter = 0
+  output_string = String.new
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array[0] + " and " + array[1]
+  else while counter < array.size - 1 do
+    array[counter]
+    output_string << array[counter] << ", "
+    counter += 1
+  end
+end
+  output_string << "and #{array[-1]}"
 end
