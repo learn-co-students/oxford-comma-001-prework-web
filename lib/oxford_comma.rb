dabd8243b74feb422ae2a92a5527d89c5446b00a
Aynalem Lenjiso <aynalem.lenjiso@gmial.com>
2016-07-28 17:14:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+	if array.length == 1
+		array.join("and")
+	elsif array.length == 2
+		array.join(" and ")
 
+	else
+	last = array.last
+	array.pop
+	x = array.join(", ")
+
+	x << ", " << "and "  + last
+     "#{x}"
+end
 end
