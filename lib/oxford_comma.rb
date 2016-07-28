c2554715e0dce5271378ac08d47dbdb4dbd3b3cc
Naveed Shah <naveed@tayybah.com>
2016-07-28 11:17:22 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    deletion = ", #{array[array.length-1]}"
+    (array.join(", ").chomp deletion) << ", and #{array[array.length-1]}"
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    "#{array[0]}"
+  end
 end
