9ab161537ce3d0a128f65acb516fa57d4d0e0aa1
Zack Adams <adams.z.d@gmail.com>
2016-07-29 15:19:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    if array.count == 1
+      array.join
+    elsif array.count == 2
+      two = array.join(" and ")
+    else
+    last = array.pop
+    string = array.join(", ")
+    "#{string}, and #{last}"
+  end
 end
