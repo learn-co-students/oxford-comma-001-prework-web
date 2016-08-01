a1b821c8662244e59a818b6dab90ce265555dc76
slindquist3 <slindquist3@gmail.com>
2016-08-01 12:41:17 -0400

    Converted an array into a string while adding an oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
+array = ["fiddleheads","okra","kohlrabi"]
 def oxford_comma(array)
-
+  if array.count == 1
+    return array[0]
+  elsif array.count == 2
+    return array[0] + " and " + array[1]
+  elsif array.count >= 3
+    newArray = []
+    i = 0
+    while i < array.count - 1
+      element = array[i]
+      newArray.push(element + ",")
+      i += 1
+    end
+    newArray << "and " + array.last
+    return newArray.join(" ")
+  end
 end
