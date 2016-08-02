7161ce1cfcd03f4fbf8e0aaab6bec107741944c8
Sarah Keller <sebethkeller@gmail.com>
2016-08-02 16:42:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  len = array.length
+  if len == 1
+    return array[0]
+  elsif len == 2
+    return array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
