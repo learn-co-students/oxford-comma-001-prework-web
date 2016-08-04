3fd64dfcc2992c45d70cb299648cfa80517f0d87
Alvin Feng <alvinfeng103@gmail.com>
2016-08-04 17:40:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+   case array.length
+   when 0
+    return ""
+   when 1
+     return array[0]
+   when 2
+     return array.join(" and ")
+    else
+      return array.take(array.length-1).join(", ") + ", and " + array.last
+  end
 end
