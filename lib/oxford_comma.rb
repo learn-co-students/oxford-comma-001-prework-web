41c26f62eb5055134b6d37fa7e73842d5e73e421
Mirim An <mimicatcodes@gmail.com>
2016-08-10 23:16:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join" and "
+  else
+    last_element = array.pop
+    array_string = array.join(", ")
+    return array_string << ", and " + "#{last_element}"
 
+end
 end
