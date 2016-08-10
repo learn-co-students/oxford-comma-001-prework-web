7a81b28705afdb8f00c30f34ea7d38b749d45f0c
Michael Williams <mw@mikewill.net>
2016-08-10 16:17:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  string = ""
+  if array.size == 1
+    string = array[0]
+  elsif array.size > 2
+    last = array.pop
+    string = array.join(", ") + ", and #{last}"
+  else
+    string = "#{array.first} and #{array.last}"
+  end
 end
