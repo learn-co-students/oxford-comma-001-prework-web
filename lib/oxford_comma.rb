8fbfdd8f30fbdd8204e22369513cb507d30ce0e2
wcpines <colby@aptible.com>
2016-08-24 16:24:00 -0400

    needed to be in lib

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
-def oxford_comma(array)
-
+def oxford_comma(input)
+  if input.length == 1
+    return input[0].strip.to_s
+  elsif input.length == 2
+    return input.join(" and ")
+  elsif input.length > 2
+    return input[0...-1].join(", ") + ", and " + input[-1]
+  else 
+    "input sucks"
+  end
 end
+
+describe "#oxford_comma" do
+  it 'returns a string without any additional fomatting when given a 1-element array' do
+    expect(oxford_comma(["kiwi"])).to eq("kiwi")
+  end
+  it "adds 'and' between elements when given a 2-element array" do
+    expect(oxford_comma(["kiwi", "durian"])).to eq("kiwi and durian")
+  end
+  it "adds commas plus a final 'and' when given a 3-element array" do
+    expect(oxford_comma(["kiwi", "durian", "starfruit"])).to eq("kiwi, durian, and starfruit")
+  end
+  it "correctly formats arrays of lengths greater than three" do
+    expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])).to eq("kiwi, durian, starfruit, mangos, and dragon fruits")
+    expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])).to eq("kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos")
+  end
+end
+
