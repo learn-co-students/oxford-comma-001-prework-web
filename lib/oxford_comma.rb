863abcec3b8dc28938275a1b18d110cc05f521c5
Megan Eloise <mev277@nyu.edu>
2016-08-25 16:53:14 +0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.length == 1
+    array2 = array.join(", ")
+    return array2
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    array.insert(-2, "and ")
+    return array[0...-1].join(", ") + array.last
+  end
 end
