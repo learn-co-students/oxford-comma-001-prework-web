f039773560236c21e1115fc7f4e6dfdde880025b
Amanda Steckler <amandajsteckler@gmail.com>
2016-08-29 16:31:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    sentence = "#{array[0]} and #{array[1]}"
+    sentence
+  else
+    last_item = array.pop
+    first_items = array.join(', ')
+    sentence = "#{first_items}, and #{last_item}"
+    sentence
+  end
 end
