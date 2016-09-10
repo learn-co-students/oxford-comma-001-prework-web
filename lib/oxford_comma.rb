811b575e9002eb1c3cd68b988443ac349ab29425
Jonathan Peterkin <jonjpeterkin@gmail.com>
2016-09-10 00:21:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  
+  if array.length < 3
+    str = array.join(" and ")
+  else
+    lastval = array.pop
+    array << "and "
+    str = array.join(", ")
+    str << lastval
+  end
+  str
 
 end
